8ee1c6d09d1a39adc5ad6ff2e48f2416e75efb84
Ema Radu <83728442+myiqis9@users.noreply.github.com>
2024-12-13 04:00:56 -0500

    placeholder!

--- js/trading.js
+++ js/trading.js
@@ -1,29 +1,45 @@
-let load = false;
-let tradelist = [];
+let invload = false;
 let tradeListEl = document.getElementById("trades");
 let page = 1;
+let jsonItems;
+
+let example = {
+    id: 0,
+    creator: "test",
+    creatoroffer: [1, 10, 10, 3]
+}
+
+let tradelist = [example];
 
 function loaded() {
+    if (invload) {
         console.log(player);
-        tradeDisplay();
+        loadTrades();
+    }
 }
 
-function tradeDisplay() {
-    let data = new FormData();
-    data.append('page', page);
+fetch('../json/items.json')
+    .then((response) => response.json())
+    .then((json) => { jsonItems = json; invload = true; loaded(); })
+    .catch(error => console.error('Error:', error));
 
-    fetch('trading.php', {
-        method: 'POST'
-    })
-        .then((response) => {
-            return response.text();
-        })
-        .then((response) => {
-            tradelist = response;
-            console.log(tradelist);
-            //loadTrades();
-        })
-}
+
+// function tradeDisplay() {
+//     let data = new FormData();
+//     data.append('page', page);
+
+//     fetch('trading.php', {
+//         method: 'POST'
+//     })
+//         .then((response) => {
+//             return response.json();
+//         })
+//         .then((response) => {
+//             tradelist = response;
+//             console.log(tradelist);
+//             loadTrades();
+//         })
+// }
 
 function loadTrades() {
     for (let trade of tradelist) {
@@ -31,9 +47,15 @@ function loadTrades() {
         tradeEl.classList.add("trade-container");
 
         const tradeBox = document.createElement("div");
-        tradeBox.classList.add("trade-box trade-left eighty");
+        tradeBox.classList.add("trade-box", "trade-left", "eighty");
 
-        for (let item of trade.creatoroffer) {
+        for (let i of trade.creatoroffer) {
+            let item;
+
+            jsonItems.forEach(j => {
+                if (i === j.id) { item = j; }
+            });
+
             const itemEl = document.createElement("div");
             itemEl.classList.add("item-box");
 
@@ -42,17 +64,18 @@ function loadTrades() {
             itemImg.src = `../assets/images/items/${item.name}.png`;
             itemImg.classList.add("item-image");
             const itemName = document.createElement("p");
-            itemName.innerHTML = `(${item.name}`;
+            itemName.innerHTML = `${item.name}`;
             itemImg.classList.add("item-name");
 
 
             //append image and name to item div
-            itemEl.append(itemImg, itemName, itemAddBtn);
+            itemEl.append(itemImg, itemName);
             tradeBox.append(itemEl);
         }
 
         const tradeBtn = document.createElement("button");
-        tradeBtn.classList.add("trade-box trade-right twenty");
+        tradeBtn.innerHTML = `Offer on Trade`;
+        tradeBtn.classList.add("trade-box", "trade-right", "twenty");
 
         tradeEl.append(tradeBox, tradeBtn);
         tradeListEl.append(tradeEl);
